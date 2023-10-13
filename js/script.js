1311ff7cc9d2a20912d45dd644f7ef49d8e101a7
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2023-10-14 01:25:56 +0200

    added some scripts

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
 import translate from "../node_modules/translate/index.min.js";
 import { words } from "../node_modules/popular-english-words/words.js";
 
-const randomEnglishWord = words[Math.trunc(Math.random() * words.length)];
+const lookTranslateBtn = document.querySelector(".eye");
+const card = document.querySelector(".card");
+const thumbUpBtn = document.querySelector(".thumb-up");
 
-const englishWord = document.querySelector(".english");
-const polishWord = document.querySelector(".polish");
+const createCard = function (pol, eng) {
+  const html = ` <div class="front-card">
+  <div class="card__title--front">${pol}</div>
+</div>
+<div class="back-card">
+  <div class="card__title--front">${eng}</div>
+</div>`;
 
-const wordsCounter = words.length;
-console.log(words.length);
-
-// const randomNum = Math.trunc(Math.random() * wordArr.length);
-// const randomWord = wordArr[randomNum];
-
-const getRandomIndex = async function (random) {
-  const text = await translate(random, "pl");
-
-  // englishWord.textContent = random;
-  // polishWord.textContent = text;
+  card.insertAdjacentHTML("beforeend", html);
+  console.log(html);
 };
 
-const translateWord = await getRandomIndex(randomEnglishWord);
-translateWord;
+const randomEnglishWord = function () {
+  const randomWord = words[Math.trunc(Math.random() * words.length)];
+  return randomWord;
+};
+
+const getRandomIndexFromEnglishArray = async function (random) {
+  const text = await translate(random, "pl");
+  return text;
+};
+
+const translateWord = await getRandomIndexFromEnglishArray(randomEnglishWord());
+
+const translateEnglishWord = function () {
+  return translateWord;
+};
+
+lookTranslateBtn.addEventListener("click", function () {
+  card.classList.toggle("rotate");
+});
+
+thumbUpBtn.addEventListener("click", function () {
+  card.textContent = "";
+  const randomEnglish = randomEnglishWord();
+  const translateEnglish = translateEnglishWord(randomEnglish);
+
+  console.log(randomEnglish, translateEnglish);
+  createCard(translateEnglish, randomEnglish);
+});
