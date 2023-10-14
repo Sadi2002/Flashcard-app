c21e2416c7f60b141f6b29ab22728b954d9dc777
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2023-10-14 10:07:02 +0200

    added almost every functionality

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
+// Importing
 import translate from "../node_modules/translate/index.min.js";
 import { words } from "../node_modules/popular-english-words/words.js";
 
+// Fetching elements
 const lookTranslateBtn = document.querySelector(".eye");
 const card = document.querySelector(".card");
 const thumbUpBtn = document.querySelector(".thumb-up");
+const thumbDownBtn = document.querySelector(".thumb-down");
+const navCounter = document.querySelector(".navigation__counter--color");
+const known = document.querySelector(".current-state__counter--known");
+const unknown = document.querySelector(".current-state__counter--unknown");
+const controlsBtns = document.querySelectorAll(".controls div");
 
+// Funtion which caontain card frame
 const createCard = function (pol, eng) {
   const html = ` <div class="front-card">
   <div class="card__title--front">${pol}</div>
@@ -16,12 +24,54 @@ const createCard = function (pol, eng) {
   card.insertAdjacentHTML("beforeend", html);
 };
 
-lookTranslateBtn.addEventListener("click", function () {
-  card.classList.toggle("rotate");
-});
+// Adding counter in nav
 
-thumbUpBtn.addEventListener("click", function () {
+let counter = 0;
+let knownCount = 0;
+let unknownCount = 0;
+
+navCounter.textContent = counter;
+
+const count = function () {
+  if (counter !== 3425) {
+    navCounter.textContent = ++counter;
+  } else {
+    counter = 0;
+    navCounter.textContent = ++counter;
+  }
+};
+
+known.textContent = counter;
+unknown.textContent = counter;
+
+const knownCounter = function () {
+  known.textContent = knownCount++;
+};
+const unknownCounter = function () {
+  unknown.textContent = ++unknownCount;
+};
+
+// Function which run only once
+const showAnimationOnce = function () {
+  thumbDownBtn.style.left = 30 + "px";
+  thumbDownBtn.style.transform = "translate(2px, 0)";
+  thumbUpBtn.style.left = "calc(100% - 65px)";
+  thumbUpBtn.style.transform = "translateX(2px, 0px)";
+
+  return () => {};
+};
+
+const handleThumbs = function (e) {
+  showAnimationOnce();
   card.textContent = "";
+
+  // if(e.target ==)
+
+  if (e.target.classList.contains("thumb-up")) {
+    knownCounter();
+  } else {
+    unknownCounter();
+  }
 
   if (card.classList.contains("rotate")) {
     card.classList.remove("rotate");
@@ -29,10 +79,27 @@ thumbUpBtn.addEventListener("click", function () {
 
   const randomEnglishWord = words[Math.trunc(Math.random() * words.length)];
 
+  const firstLetterUp = randomEnglishWord.charAt(0).toUpperCase();
+
+  const randomEngWord = firstLetterUp + randomEnglishWord.slice(1);
+
   const translateEnglishWord = async function () {
-    const text = await translate(randomEnglishWord, "pl");
+    const text = await translate(randomEngWord, "pl");
     return text;
   };
 
-  translateEnglishWord().then((resp) => createCard(resp, randomEnglishWord));
+  translateEnglishWord().then((resp) => createCard(resp, randomEngWord));
+
+  count();
+};
+
+// Function which toggle animation on card, when you press eye button
+lookTranslateBtn.addEventListener("click", function () {
+  card.classList.toggle("rotate");
 });
+
+// Function which show generate text
+thumbUpBtn.addEventListener("click", handleThumbs);
+
+// Function which show which words you can't
+thumbDownBtn.addEventListener("click", handleThumbs);
